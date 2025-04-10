6b7dd0facc9d924dfc654b03723596c366a71481
Marek Kovarik <272838@vutbr.cz>
2025-04-10 09:45:13 +0200

    xd

--- searching.py
+++ searching.py
@@ -1,5 +1,6 @@
 import os
 import json
+
 # get current working directory path
 cwd_path = os.getcwd()
 
@@ -11,11 +12,36 @@ def read_data(file_name, field):
         sequential_data = data[field]
     return sequential_data
 
-print(read_data("sequential.json","unordered_numbers"))
+def linear_search(sequence,number):
+    index = 0
+    kanal = {}
+    pozice = []
+    for i in sequence:
+        index +=1
+        if i == number:
+            pozice.append(index)
+    kanal["index"] = pozice
+    kanal["pocet"] = sequence.count(number)
+    return kanal
 
-def main():
-    pass
+def pattern_search(sequence,vzor):
+    kanal = {}
+    pozice = []
+    delka = len(vzor)
+    for i in range(len(sequence) - delka + 1):
+        if sequence[i:i + delka] == vzor:
+            pozice.append(i)
+    kanal["index"] = pozice
+    return kanal
+
+
+def main(file_name,field,number,vzor):
+    a = read_data(file_name,field)
+ #   b = linear_search(a,number)
+    c = pattern_search(a,vzor)
+    return print(f"{c}")
+
 
 
 if __name__ == '__main__':
-    main()
+    main("sequential.json","dna_sequence", 0, "ATA")
